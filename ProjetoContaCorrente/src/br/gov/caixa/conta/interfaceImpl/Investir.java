79e67719be1af4514abd13b1e674f90b1dc864f5
thiagotatagiba <tatagiba.thiago@gmail.com>
2024-04-06 11:30:05 -0300

    Finalização da segunda etapa do projeto.

--- ProjetoContaCorrente/src/br/gov/caixa/conta/interfaceImpl/Investir.java
+++ ProjetoContaCorrente/src/br/gov/caixa/conta/interfaceImpl/Investir.java
@@ -1,6 +1,7 @@
 package br.gov.caixa.conta.interfaceImpl;
 
 import br.gov.caixa.conta.conta.Conta;
+import br.gov.caixa.conta.conta.pessoafisica.ContaCorrentePF;
 import br.gov.caixa.conta.conta.pessoafisica.ContaInvestimentoPF;
 import br.gov.caixa.conta.conta.pessoajuridica.ContaCorrentePJ;
 import br.gov.caixa.conta.conta.pessoajuridica.ContaInvestimentoPJ;
@@ -10,11 +11,11 @@ import static br.gov.caixa.conta.constantes.Constantes.TARIFA_SAQUE_PF;
 import static br.gov.caixa.conta.constantes.Constantes.TARIFA_SAQUE_PJ;
 
 public class Investir implements br.gov.caixa.conta.interfaces.Investir {
-    public Boolean executar(Conta origem, ContaInvestimentoPF destino, double valor) {
+    public Boolean executar(ContaCorrentePF origem, ContaInvestimentoPF destino, double valor) {
         return executar(origem, destino, valor, TARIFA_SAQUE_PF);
     }
 
-    public Boolean executar(Conta origem, ContaInvestimentoPJ destino, double valor) {
+    public Boolean executar(ContaCorrentePJ origem, ContaInvestimentoPJ destino, double valor) {
         return executar(origem,destino,valor,TARIFA_SAQUE_PJ);
     }
 
